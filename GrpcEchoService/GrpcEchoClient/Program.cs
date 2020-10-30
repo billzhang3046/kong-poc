52bc1c74d1b6165afaf97b0bd74df991f5c4fefc
Zhang <I50617@verisk.com>
2020-10-30 10:40:34 -0400

    Add sample grpc service and client

---
+++ GrpcEchoService/GrpcEchoClient/Program.cs
@@ -0,0 +1,71 @@
+﻿using Grpc.Net.Client;
+using GrpcEchoService;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Security;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace GrpcEchoClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, SslPolicyErrors) => true;
+
+            if (args[0].ToLower() == "g")
+                TestGrpc(Convert.ToInt32(args[1]));
+            else
+                await TestRestful(Convert.ToInt32(args[1]));
+        }
+
+        private static async Task TestRestful(int port)
+        {
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+            // Pass the handler to httpclient(from you are calling api)
+            HttpClient client = new HttpClient(clientHandler);
+
+            client.BaseAddress = new Uri($"https://localhost:{port}");
+
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            HttpResponseMessage response = await client.GetAsync("loss/1/result/event");
+            response.EnsureSuccessStatusCode();
+            stopwatch.Stop();
+            Console.WriteLine($"{stopwatch.ElapsedMilliseconds}");
+        }
+
+        private static void TestGrpc(int port)
+        {
+            var httpHandler = new HttpClientHandler();
+            // Return `true` to allow certificates that are untrusted/invalid
+            httpHandler.ServerCertificateCustomValidationCallback =
+                HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+            var httpClient = new HttpClient(httpHandler);
+
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+            var endpoint = $"https://localhost:{port}";
+            var channel = GrpcChannel.ForAddress(endpoint, new GrpcChannelOptions { HttpClient = httpClient });
+            var client = new Loss.LossClient(channel);
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var result = client.GetEventResult(new GetResultRequest
+            {
+                AnalysisSid = 1
+            });
+            stopwatch.Stop();
+            Console.WriteLine($"{stopwatch.ElapsedMilliseconds}");
+        }
+    }
+}
---
+++ GrpcEchoService/GrpcEchoService/Services/LossService.cs
@@ -0,0 +1,57 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrpcEchoService.Services
+{
+    public class LossService : Loss.LossBase
+    {
+        private readonly ILogger<LossService> _logger;
+        public LossService(ILogger<LossService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CountIntegerResponse> CountInteger(CountIntegerRequest request, ServerCallContext context)
+        {
+            var response = new CountIntegerResponse();
+            for (int i = 0; i < request.Count; i++)
+            {
+                response.Numbers.Add(1);
+            }
+
+            return Task.FromResult(response);
+        }
+
+        public override Task<GetEventResultResponse> GetEventResult(GetResultRequest request, ServerCallContext context)
+        {
+            var random = new Random();
+
+            var response = new GetEventResultResponse
+            {
+                AnalysisSid = request.AnalysisSid,
+            };
+
+            for (int i = 0; i < request.AnalysisSid; i++)
+            {
+                response.Result.Add(new EventResult
+                {
+                    PerilSetCode = 4,
+                    YearId = random.Next(request.AnalysisSid),
+                    ModelCode = random.Next(255),
+                    EventId = random.Next(),
+                    Gross = random.NextDouble(),
+                    GroundUp = random.NextDouble(),
+                    Retained = random.NextDouble(),
+                    PreCatNet = random.NextDouble()
+                });
+            }
+
+            return Task.FromResult(response);
+        }
+    }
+}
